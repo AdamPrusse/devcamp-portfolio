a9440037a0aa5e48fd0375f9f958ffd19df8ca73
Adam Prusse <adamprusse@Adams-MBP.ad.uvu.edu>
2017-12-04 11:06:14 -0700

    Configured custom routes for pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
   resources :portfolios
-
-
-  get 'about', to: 'pages#about'
-
-  get 'contact', to: 'pages#contact'
+  
+    get 'about', to: 'pages#about'
+	get 'contact', to: 'pages#contact'
 
   resources :blogs
 
