a9e0bc3cf4173cb5ee4ed2eafdd340361a73592a
Adam Prusse <adamprusse@Adams-MBP.ad.uvu.edu>
2017-12-12 10:42:46 -0700

    Integrated new gem into rails app for copyright

--- Gemfile
+++ Gemfile
@@ -54,3 +54,5 @@ gem 'friendly_id', '~> 5.2', '>= 5.2.3'
 gem 'devise', '~> 4.3'
 gem 'bootstrap', '~> 4.0.0.beta2.1'
 gem 'jquery-rails', '~> 4.3', '>= 4.3.1'
+
+gem 'devcamp_view_tool', '~> 0.1.0'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,5 +15,9 @@ module ApplicationHelper
 			content_tag(:p, greeting, class: "source-greeting")
 		end
 	end
+
+	def copyright_generator
+	DevcampViewTool::Renderer.copyright 'Adam Prusse', 'All rights reserved'
+	end	
 end
 
