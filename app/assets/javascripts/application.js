808316ae89e6a1ba0620b380265aeeaaf2807337
Adam Prusse <adamprusse@Adams-MBP.ad.uvu.edu>
2017-12-22 14:54:00 -0700

    Changed my application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,14 +10,14 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
+//= require jquery3
+//= require popper
+//= require rails-ujs
 //= require jquery-ui
 //= require html.sortable
 //= require cocoon
 //= require gritter
 //= require cable
-//= require rails-ujs
 //= require turbolinks
 //= require_tree .
 //= require bootstrap-sprockets
